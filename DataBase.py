7423bb0ad1ec7afdf1bde13ff181c91977e99fd2
Ehsan <ehsanhamzekhani80@gmail.com>
2024-03-14 01:12:55 +0330

    add admin,purchase,manager table and update other tables

--- DataBase.py
+++ DataBase.py
@@ -1,4 +1,5 @@
 from DataBase_Connection import get_database_connection
+from datetime import datetime
 
 # Get the database connection
 mydb, cursor = get_database_connection()
@@ -87,3 +88,86 @@ mydb, cursor = get_database_connection()
 # # Commit the transaction
 # mydb.commit()
 ######################################################################################
+# SQL statement to add the column                                                    # SQL statement to add the column
+# alter_query = """
+# ALTER TABLE books
+# ADD COLUMN catalog_flag INT DEFAULT 1;
+# """
+#
+# # Execute the SQL statement
+# cursor.execute(alter_query)
+# CREATE TABLE PURCHASE                                                 # CREATE TABLE PURCHASE
+# create_table_query = """
+# CREATE TABLE IF NOT EXISTS purchases (
+#     purchase_id INT AUTO_INCREMENT PRIMARY KEY,
+#     user_id INT,
+#     ISBN VARCHAR(20),
+#     book_name VARCHAR(50),
+#     purchase_date DATETIME,
+#     credit_card_type VARCHAR(50),
+#     credit_card_number VARCHAR(20),
+#     purchase_status VARCHAR(20),
+#     admin_fullName VARCHAR(50),
+#     admin_id INT,
+#     FOREIGN KEY (user_id) REFERENCES users(user_id)
+# );
+# """
+#
+# # Execute the SQL statement
+# cursor.execute(create_table_query)
+# # CREATE TABLE ADMIN                                                    # CREATE TABLE ADMIN
+# create_table_query = """
+# CREATE TABLE IF NOT EXISTS admin (
+#     admin_id INT AUTO_INCREMENT PRIMARY KEY,
+#     username VARCHAR(50) UNIQUE,
+#     password VARCHAR(50),
+#     msg_box TEXT,
+#     purchase_actions TEXT,
+#     worked_purchase_ids TEXT,
+#     orders TEXT,
+#     book_operations TEXT,
+#     catalog_changes TEXT
+# );
+# """
+# cursor.execute(create_table_query)
+# SQL statement to add the date_in_out column
+# alter_query = """
+# ALTER TABLE admin
+# ADD COLUMN date_in_out DATETIME;
+# """
+#
+# # Execute the SQL statement
+# cursor.execute(alter_query)
+
+# SQL statement to insert a new admin
+insert_query = """
+    INSERT INTO admin (username, password, msg_box, purchase_actions, worked_purchase_ids, orders, book_operations, catalog_changes, date_in_out)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+"""
+
+# Define the values for the new admin
+new_admin_values = ('ehsanAdmin', 'ehsanAdmin', 'new_admin_msg_box', 'new_admin_purchase_actions', 'new_admin_worked_purchase_ids', 'new_admin_orders', 'new_admin_book_operations', 'new_admin_catalog_changes', datetime.now())
+
+# Execute the INSERT query with the values
+cursor.execute(insert_query, new_admin_values)
+
+# Commit the transaction
+mydb.commit()
+###########################################################################################################33
+# SQL statement to create the manager table
+# CREATE TABLE manager                                                            # CREATE TABLE manager
+# create_table_query = """
+# CREATE TABLE IF NOT EXISTS manager (
+#     manager_id INT AUTO_INCREMENT PRIMARY KEY,
+#     msg_box TEXT,
+#     full_name VARCHAR(50),
+#     national_id VARCHAR(10),
+#     date_in_out DATETIME,
+#     allowed_purchases TEXT,
+#     orders_to_add TEXT,
+#     book_oprations TEXT
+# );
+# """
+#
+# # Execute the SQL statement
+# cursor.execute(create_table_query)
